d11be5225417cf67be95d36658692f7d2fdcab12
Tim Weyel <tweyel@gmail.com>
2021-04-13 15:06:47 -0700

    finished requests to serve page data

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -91,6 +92,22 @@ app.post('/api/animals', (req, res) => {
   const animal = createNewAnimal(req.body, animals);
 
   res.json(animal);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
